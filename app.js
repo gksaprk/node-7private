270beeab0d90b41a2afc4db6456329142e205a18
gksaprk <gksaprk@gmail.com>
2024-02-05 20:35:54 +0900

    최종본

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ dotenv.config();
 import express from "express";
 import cookieParser from "cookie-parser";
 import UsersRouter from "./routers/users.router.js";
+import LogMiddleware from "./middlewares/log.middleware.js";
 import DocumentsRouter from "./routers/documents.router.js";
+import ErrorHandlingMiddleware from "./middlewares/error-handling-middleware.js";
 
 const app = express();
 const port = process.env.PORT || 3020;
-const databaseUrl = process.env.DATABASE_URL;
-console.log(databaseUrl);
+// const databaseUrl = process.env.DATABASE_URL;
+// console.log(databaseUrl);
 
+app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-// 예시: public 디렉터리의 파일을 정적 파일로 서빙
-app.use(express.static("public"));
-
 app.use("/api", UsersRouter, DocumentsRouter);
+app.use(ErrorHandlingMiddleware);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
--- routers/users.router.js
+++ routers/users.router.js
@@ -3,14 +3,14 @@ import { prisma } from "../models/index.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import authMiddleware from "../middlewares/need-signin.middlware.js";
+import { Prisma } from "@prisma/client";
 
 const router = express.Router();
 
 /** 사용자 회원가입 API **/
 router.post("/sign-up", async (req, res, next) => {
   try {
-    const { email, password, passwordCheck, name, age, gender, profileImage } =
-      req.body;
+    const { email, password, name, age, gender, profileImage } = req.body;
     const isExistUser = await prisma.users.findFirst({
       where: {
         email,
@@ -25,7 +25,7 @@ router.post("/sign-up", async (req, res, next) => {
         .status(400)
         .json({ message: "비밀번호는 최소 6자 이상입니다." });
     }
-    if (password !== passwordCheck) {
+    if (password !== password) {
       return res
         .status(400)
         .json({ message: " 비밀번호 확인이 일치하지 않습니다." });
@@ -33,74 +33,92 @@ router.post("/sign-up", async (req, res, next) => {
 
     // Users 테이블에 사용자를 추가합니다. + hash
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.users.create({
-      data: { email, password: hashedPassword, passwordCheck },
-    });
-    // UserInfos 테이블에 사용자 정보를 추가합니다.
-    const userInfo = await prisma.userInfos.create({
-      data: {
-        userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
-        name,
-        age,
-        gender,
-        profileImage,
+
+    // 트랜잭션
+    const [user, userInfo] = await prisma.$transaction(
+      async (tx) => {
+        const user = await tx.users.create({
+          data: { email, password: hashedPassword },
+        });
+        // UserInfos 테이블에 사용자 정보를 추가합니다.
+        const userInfo = await tx.userInfos.create({
+          data: {
+            userId: user.userId, // 생성한 유저의 userId를 바탕으로 사용자 정보를 생성합니다.
+            name,
+            age,
+            gender,
+            profileImage,
+          },
+        });
+        return [user, userInfo];
       },
-    });
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+      }
+    );
 
     return res.status(201).json({ message: "회원가입이 완료되었습니다." });
-  } catch (error) {
-    return res.status(500).json({ errorMessage: "회원가입에 실패하였습니다." });
+  } catch (err) {
+    next(err);
   }
 });
 
 /** 로그인 API **/
 router.post("/sign-in", async (req, res, next) => {
-  const { email, password } = req.body;
-  const user = await prisma.users.findFirst({ where: { email } });
+  try {
+    const { email, password } = req.body;
+    const user = await prisma.users.findFirst({ where: { email } });
 
-  if (!user)
-    return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
-  // 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
-  else if (!(await bcrypt.compare(password, user.password)))
-    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+    if (!user)
+      return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
+    // 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
+    else if (!(await bcrypt.compare(password, user.password)))
+      return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
 
-  // 로그인에 성공하면, 사용자의 userId를 바탕으로 토큰을 생성합니다.
-  const token = jwt.sign(
-    {
-      userId: user.userId,
-    },
-    "custom-secret-key",
-    { expiresIn: "12h" } // 12시간 유효
-  );
+    // 로그인에 성공하면, 사용자의 userId를 바탕으로 토큰을 생성합니다.
+    const token = jwt.sign(
+      {
+        userId: user.userId,
+      },
+      "custom-secret-key",
+      { expiresIn: "12h" } // 12시간 유효
+    );
 
-  // authotization 쿠키에 Berer 토큰 형식으로 JWT를 저장합니다.
-  res.cookie("authorization", `Bearer ${token}`);
-  return res.status(200).json({ message: "로그인 성공하였습니다." });
+    // authotization 쿠키에 Berer 토큰 형식으로 JWT를 저장합니다.
+    res.cookie("authorization", `Bearer ${token}`);
+    return res.status(200).json({ message: "로그인 성공하였습니다." });
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get("/users", authMiddleware, async (req, res, next) => {
-  const { userId } = req.user;
+  try {
+    const { userId } = req.user;
 
-  const user = await prisma.users.findFirst({
-    where: { userId: +userId },
-    select: {
-      userId: true,
-      email: true,
-      createdAt: true,
-      updatedAt: true,
-      userInfos: {
-        // 1:1 관계를 맺고있는 UserInfos 테이블을 조회합니다.
-        select: {
-          name: true,
-          age: true,
-          gender: true,
-          profileImage: true,
+    const user = await prisma.users.findFirst({
+      where: { userId: +userId },
+      select: {
+        userId: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+        userInfos: {
+          // 1:1 관계를 맺고있는 UserInfos 테이블을 조회합니다.
+          select: {
+            name: true,
+            age: true,
+            gender: true,
+            profileImage: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  return res.status(200).json({ data: user });
+    return res.status(200).json({ data: user });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
